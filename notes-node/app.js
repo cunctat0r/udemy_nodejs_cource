8f3802dfa60a0e9c93dffd1ff14a3cc0a8271546
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-11 12:37:28 +0000

    Experimуnts with lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,14 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
+console.log(_.isString(true));
+console.log(_.isString('Slava'));
+
+var filteredArray = _.uniq(['Slava', 1, 'Slava', 1, 2, 3, 4]);
+console.log(filteredArray);
 
 var res = notes.addNote();
 console.log(res);
