3e84ab894b611605a39ea24e4cb58c10bf87f58d
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-12 08:39:07 +0000

    Working with command line parameters

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,4 +5,16 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+var command = process.argv[2];
 
+console.log('Command:', command);
+
+if (command === 'add') {
+  console.log('Adding new note.');
+} else if (command === 'list') {
+  console.log('Listing all notes.');
+} else if (command === 'delete') {
+  console.log('Deleting note.');
+} else {
+  console.log('Command not recognized.');
+}
