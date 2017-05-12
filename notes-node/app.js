139deea82ce09a1fcfc14ef964dffcb3056b14f2
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-12 11:25:39 +0000

    Determite wheter note is added or not

--- notes-node/app.js
+++ notes-node/app.js
@@ -14,7 +14,14 @@ console.log('Yargs:', argv);
 
 
 if (command === 'add') {
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note added:');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log('Not added. Please select another title.')
+  }
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
