9946629ac33b8c14fcb745063fcaaae23960835d
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-15 06:02:49 +0000

    Set up all the yargs arguments

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,19 +5,30 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs
-  .command('add', 'Add a new note', {
-    title: {
+const titleOptions = {
       describe: 'Title of note',
       demand: true,
       alias: 't'
-    },
-    body: {
+};
+
+const bodyOptions = {
       describe: 'Body of note',
       demand: true,
       alias: 'b'
-    }  
+};
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions 
   })
+  .command('read', 'Read note with specified title', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove note with specified title', {
+    title: titleOptions
+  })
+  .command('list', 'Show all notes')
   .help()
   .argv;
 var command = argv._[0];
