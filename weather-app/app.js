d1f73a091a5dc6d6c1ff0ed69350ecf023c5e1d7
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-22 07:51:57 +0000

    Set up yargs

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,22 @@
 const request = require('request');
+const yargs = require('yargs');
+
+// 68 Studencheskaya street Engels
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for.',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=68%20Studencheskaya%20street%20Engels',
