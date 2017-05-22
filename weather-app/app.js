9b929fd3577fdaed1be164902e245af59a5ba394
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-22 07:21:16 +0000

    Print addres, latitude and longitude

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=68%20Oktyabrskaya%20street%20Kvasnikovka',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(body.results[0].formatted_address);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
