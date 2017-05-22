1ac185bc279a3bde557301ba1472f449f779e759
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-22 06:48:49 +0000

    Make request to geolocate google api

---
+++ weather-app/app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=68%20Oktyabrskaya%20street%20Kvasnikovka',
+  json: true
+}, (error, response, body) => {
+  console.log(body);
+});
