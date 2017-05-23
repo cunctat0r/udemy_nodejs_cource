be28dd28eb0ddba9e46a1e9c7f5a24fb4af7ad66
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-23 11:58:30 +0000

    Testing with expect

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,24 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-
-  if (res !== 44) {
-    throw new Error(`Expected 44, got ${res}`);
-  }
+  
+  expect(res).toBe(44).toBeA('number');
 });
 
 it ('should square number', () => {
   var res = utils.square(15);
 
-  if (res !== 225) {
-    throw new Error(`Expected 225, got ${res}`);
-  }
+  expect(res).toBe(225).toBeA('number');
 });
+
+it ('should set firstName and lastName', () => {
+  var user = {age: 36};
+  var fullName = 'Slava Petrov';
+
+  expect(utils.setName(user, fullName))
+    .toInclude({firstName: 'Slava'})
+    .toInclude({lastName: 'Petrov'})
+    .toBeA('object');
+})
