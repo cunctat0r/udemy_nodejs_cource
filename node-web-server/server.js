f4d1845c19191f501000dedb80bd9a77de04380f
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-23 08:23:50 +0000

    Create other helper function

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -11,6 +11,10 @@ hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (request, response) => {
   response.render('home.hbs', {
     pageTitle: 'Home Page',
