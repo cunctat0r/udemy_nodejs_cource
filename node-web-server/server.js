08abac142e82f265ba80e96500574701075d2e6e
Slava Petrov <slavik--petrov@yandex.ru>
2017-05-23 08:19:51 +0000

    Create helper function

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -7,10 +7,13 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
 app.get('/', (request, response) => {
   response.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome to my home page!'
   })
 });
@@ -18,8 +21,7 @@ app.get('/', (request, response) => {
 app.get('/about', (req, res) => {
   //res.send('About page');
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
