07693dd13385df75785e6671a0e9e621c51ab6ce
Slava Petrov <slavik--petrov@yandex.ru>
2017-06-05 09:49:01 +0000

    Install express

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -7,10 +7,24 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+}); 
+
 it ('should square number', () => {
   var res = utils.square(15);
 
   expect(res).toBe(225).toBeA('number');
+});
+
+it('should async square number', (done) => {
+  utils.asyncSquare(4, (sqr) => {
+    expect(sqr).toBe(16).toBeA('number');
+    done();
+  });
 });
 
 it ('should set firstName and lastName', () => {
