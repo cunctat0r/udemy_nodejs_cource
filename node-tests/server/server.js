003e9b7f3e94f325b61c68d0ddffa071336a0178
Slava Petrov <slavik--petrov@yandex.ru>
2017-06-05 10:35:25 +0000

    Test express app using supertest

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,7 +3,26 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'John',
+    age: 24
+  }, 
+  {
+    name: 'Jane',
+    age: 22
+  }, 
+  {
+    name: 'Mike',
+    age: 32
+  }]);
 });
 
 app.listen(3000);
+module.exports.app = app;
